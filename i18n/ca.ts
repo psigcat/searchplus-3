f77d53c04b4352789177e87f22f81a3625a3b762
Luigi Pirelli <luipir@gmail.com>
2015-06-24 13:58:34 +0200

    added extraction of cat string for qt4 linguistic trasnlation

---
+++ i18n/ca.ts
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SearchPlus</name>
+    <message>
+        <location filename="search_plus.py" line="192"/>
+        <source>&amp;searchplus</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus.py" line="178"/>
+        <source>Cercador toponom&#xc3;&#xa0;stica</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>searchPlusDockWidget</name>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="177"/>
+        <source>Cercadors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="180"/>
+        <source>Carrerer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="178"/>
+        <source>Carrer:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="179"/>
+        <source>N&#xc3;&#xba;mero:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="182"/>
+        <source>Top&#xc3;&#xb2;nims</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="181"/>
+        <source>Desplegueu per seleccionar un top&#xc3;&#xb2;nim; escriviu per constr&#xc3;&#xa8;nyer la cerca (per exemple, &quot;can&quot;):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="185"/>
+        <source>Equipaments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="183"/>
+        <source>Tipus:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="184"/>
+        <source>Equipament:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="187"/>
+        <source>Cadastre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="186"/>
+        <source>Introdu&#xc3;&#xaf;u la refer&#xc3;&#xa8;ncia cadastral i premeu Acceptar (exemple: &apos;5123501DF1952S&apos;):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="191"/>
+        <source>UTM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="188"/>
+        <source>Introdu&#xc3;&#xaf;u el parell de coordenades i premeu Acceptar (exemple: X &apos;415165&apos;, Y &apos;4592355&apos;):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="189"/>
+        <source>X:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="190"/>
+        <source>Y:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="search_plus_dialog_base_ui.py" line="192"/>
+        <source>Recerca</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
