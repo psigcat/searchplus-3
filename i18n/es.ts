348528358b4084252d959c3cce6d67c4af9c3b4c
carlospsig <carlos.lopez@psig.es>
2015-06-25 17:55:37 +0200

    translated to Spanish

--- i18n/es.ts
+++ i18n/es.ts
@@ -17,7 +17,7 @@
     </message>
     <message>
         <location filename="search_plus.py" line="178"/>
-        <source>Cercador toponom&#xc3;&#xa0;stica</source>
+        <source>Buscador avanzado</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -25,17 +25,17 @@
     <name>searchPlusDockWidget</name>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="177"/>
-        <source>Cercadors</source>
+        <source>Buscador</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="180"/>
-        <source>Carrerer</source>
+        <source>Callejero</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="178"/>
-        <source>Carrer:</source>
+        <source>Calle:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -45,37 +45,37 @@
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="182"/>
-        <source>Top&#xc3;&#xb2;nims</source>
+        <source>Top&#xc3;&#xb2;nimo</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="181"/>
-        <source>Desplegueu per seleccionar un top&#xc3;&#xb2;nim; escriviu per constr&#xc3;&#xa8;nyer la cerca (per exemple, &quot;can&quot;):</source>
+        <source>Desplegar para seleccionar un top&#xc3;&#xb2;nimo; escribir para restringir la busqueda (por ejemplo, &quot;can&quot;):</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="185"/>
-        <source>Equipaments</source>
+        <source>Equipamientos</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="183"/>
-        <source>Tipus:</source>
+        <source>Tipo:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="184"/>
-        <source>Equipament:</source>
+        <source>Equipamiento:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="187"/>
-        <source>Cadastre</source>
+        <source>Catastro</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="186"/>
-        <source>Introdu&#xc3;&#xaf;u la refer&#xc3;&#xa8;ncia cadastral i premeu Acceptar (exemple: &apos;5123501DF1952S&apos;):</source>
+        <source>Introduce la referencia catastral y pulsa Aceptar (ejemplo: &apos;5123501DF1952S&apos;):</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -85,7 +85,7 @@
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="188"/>
-        <source>Introdu&#xc3;&#xaf;u el parell de coordenades i premeu Acceptar (exemple: X &apos;415165&apos;, Y &apos;4592355&apos;):</source>
+        <source>Introduce un par de coordenadas y pulsa Aceptar (ejemplo: X &apos;415165&apos;, Y &apos;4592355&apos;):</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -100,7 +100,7 @@
     </message>
     <message>
         <location filename="search_plus_dialog_base_ui.py" line="192"/>
-        <source>Recerca</source>
+        <source>Busca</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
