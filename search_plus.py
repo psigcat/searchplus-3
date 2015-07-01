64d3d97c0a7d4844256e4583492d26483b26495f
Luigi Pirelli <luipir@gmail.com>
2015-07-01 12:23:47 +0200

    added control of cancel insertig credential

--- search_plus.py
+++ search_plus.py
@@ -307,7 +307,10 @@ class SearchPlus(QObject):
         DATABASE_USER = qgisSettings.value(root+"username", '')
         DATABASE_PWD = qgisSettings.value(root+"password", '')
         
-        (ok, DATABASE_USER, DATABASE_PWD) = QgsCredentials.instance().get( "", DATABASE_USER, DATABASE_PWD );
+        (ok, DATABASE_USER, DATABASE_PWD) = QgsCredentials.instance().get( "", DATABASE_USER, DATABASE_PWD )
+        if not ok:
+            return
+        
         self.uri = QgsDataSourceURI()
         self.uri.setConnection(DATABASE_HOST, DATABASE_PORT, DATABASE_NAME, DATABASE_USER, DATABASE_PWD)
         
